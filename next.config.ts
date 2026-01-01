57ccd3d3bb21102f7aa0d0878e9280d6b5a54a87
thebluevalley <bluevalley1985@gmail.com>
2026-01-01 11:29:55 +0000

    1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 确保这里没有 distDir: 'old-world/.next' 之类的奇怪设置
+  // 确保没有 output: 'export' (除非你只要静态页面且不想要 API)
 };
 
 export default nextConfig;
